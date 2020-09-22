484327e39a517593977817c86168a856e9231147
Dhananjay Jakhadi <dhananjay.jakhadi1@gmail.com>
2020-09-21 23:23:06 -0400

    Test case moved

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,7 +1,6 @@
 import { $, $$, browser, ExpectedConditions, element, by } from 'protractor';
 
 describe('When: I use the reading list feature', () => {
-
   beforeEach(async () => {
     await browser.get('/');
     await browser.wait(
@@ -21,43 +20,38 @@ describe('When: I use the reading list feature', () => {
     );
   });
 
-  it('Then: I should be able to add book in my reading list', async () => {
-    const form = await $('form');
-    const input = await $('input[type="search"]');
-    await input.sendKeys('javascript');
-    await form.submit();
-    const readinglistAdd = await $('#wantToRead_2');
-    await readinglistAdd.click();
-    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
-    await readingListToggle.click();
-    const items = await $$('[data-testing="reading-list-container"]');
-    expect(items.length).toEqual(1, 'at least one');
-  });
+  describe('for submit form, ', () => {
+    beforeEach(async () => {
+      const form = await $('form');
+      const input = await $('input[type="search"]');
+      await input.sendKeys('javascript');
+      await form.submit();
+    });
+    it('Then: I should be able to add book in my reading list', async () => {
+      const readinglistAdd = await $('#wantToRead_2');
+      await readinglistAdd.click();
+      const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+      await readingListToggle.click();
+      const items = await $$('[data-testing="reading-list-container"]');
+      expect(items.length).toEqual(1, 'at least one');
+    });
 
-  it('Then: I should be able to view snack bar and add item', async () => {
-    const form = await $('form');
-    const input = await $('input[type="search"]');
-    await input.sendKeys('javascript');
-    await form.submit();
-    const readinglistAdd = await $('#wantToRead_3');
-    await readinglistAdd.click();
-    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
-    await readingListToggle.click();
-    const items = await $$('[data-testing="reading-list-container"]');
-    expect(items.length).toEqual(1, 'at least one');
-  });
+    it('Then: I should be able to view snack bar and add item', async () => {
+      const readinglistAdd = await $('#wantToRead_3');
+      await readinglistAdd.click();
+      const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+      await readingListToggle.click();
+      const items = await $$('[data-testing="reading-list-container"]');
+      expect(items.length).toEqual(1, 'at least one');
+    });
 
-  it('Then: I should able to remove my action from snack bar', async () => {
-    const form = await $('form');
-    const input = await $('input[type="search"]');
-    await input.sendKeys('javascript');
-    await form.submit();
-    const readinglistAdd = await $('#wantToRead_5');
-    await readinglistAdd.click();
-
-    const readingListToggle = await $('[data-testing="toggle-reading-list"]');
-    await readingListToggle.click();
-    const items = await $$('[data-testing="reading-list-container"]');
-    expect(items.length).toEqual(1, 'at least one');
+    it('Then: I should able to remove my action from snack bar', async () => {
+      const readinglistAdd = await $('#wantToRead_5');
+      await readinglistAdd.click();
+      const readingListToggle = await $('[data-testing="toggle-reading-list"]');
+      await readingListToggle.click();
+      const items = await $$('[data-testing="reading-list-container"]');
+      expect(items.length).toEqual(1, 'at least one');
+    });
   });
 });
